860a6e6dd96ed1cc7801c7f2bf73f0832ebf6f15
Jeffrey Esedo <jiesedo@gmail.com>
2025-07-25 08:28:19 +0100

    Update: import beautiful soup

--- scripts/extract_html.py
+++ scripts/extract_html.py
@@ -3,15 +3,25 @@ import requests
 import pathlib
 from bs4 import BeautifulSoup
 
-def main():
-    print(get_url())
+def main(url):
+    print(get_url(url))
 
-def get_url():
-    r= requests.get("https://www.afro.who.int/")
+
+def get_url(url):
+    # send a get request to url
+    r= requests.get(url)
+    r.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+
     url_status= r.status_code
-    return url_status
+    # url_headers= r.headers['content-type']
+    url_content= r.text
 
+    # Parse the HTML content
+    soup= BeautifulSoup(url_content, 'html.parser')
+
+    return url_status, soup
 
 
 if __name__=="__main__":
-    main()
+    url= "https://www.afro.who.int/"
+    main(url)
