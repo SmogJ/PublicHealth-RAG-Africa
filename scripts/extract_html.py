740446759e80f606cdfbc15cb5445281f912c9fe
Jeffrey Esedo <jiesedo@gmail.com>
2025-08-19 11:13:00 +0100

    Update: get_content_url from highlight page

--- scripts/extract_html.py
+++ scripts/extract_html.py
@@ -5,63 +5,81 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 
 
-def main(url):
-    url_content= get_url(url)
-    print(parse_url(url_content[0], url_content[2]))
+def main():
+    url= "https://www.afro.who.int/news/feature_stories"
+    nav_url= get_nav_urls(url)
+    print(get_contetn_url(nav_url))
     
 
 
-def get_url(url):
+def get_nav_urls(url):
     try:
         # send a get request to url
         r= requests.get(url)
         r.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
 
         url_status= r.status_code
-        url_headers= r.headers['content-type']
+        print(url_status)
         url_content= r.text # get url content as text
+      
 
-        return url_status, url_headers, url_content
+        # Parse html content using beautiful soup
+        # find main content of the feature stories page
+        soup = BeautifulSoup(url_content, 'html.parser')   
+        main_content= soup.find("div", "region region-content")
+
+
+        # Find the last page number and use it to loop through the pages of the URL
+        last_page= main_content.find("li", "pager__item pager__item--last")
+        last_a_num= int(last_page.find("a").get("href").strip("?page="))
+        
+        # loop the throught the total number of pages in the 
+        nav_urls= []
+        for num in range(0, last_a_num + 1):
+            nav_urls.append(f"https://www.afro.who.int/news/feature_stories?page={num}")
+        for url in nav_urls:
+            return url
+
     except requests.exceptions.RequestException as e:
         print(f"Error fetching {url}: {e}")
         return None
 
-def parse_url(status, content):
-    print(f"Website status: {status}")
-
+def get_contetn_url(url):
     # --- Strategy for extracting main content ---
-        # The Feature Store Page contents highlights of news and stories.
-        # This news and story content are found in a div (div class="view-content"), that contains a common div (div class="row views-row").
-        # The row views-row contains common tags, which are:
+        # The Feature Stories Page contents highlights of news/stories.
+        # They are all under the a div (div class="view-content"), but will be accessed through a div with class name="region region-content" 
+        # to get the content for each news/stories, the div (div class="row views-row") contains the followung tags and attributes.
         #   - h3 tag (class="teaser-full__title"), which we use to get the the a tag.
-        #   - the a tag has the link, and the title of the story, we'll use this to get to the main html content.
-        #   - a time elememt containing the date and time of publication, we'll use to get the data and time of publication of the story.
+        #   - the a tag has the link/path to the main content, and the title of the story, we'll use this to get to the main html content.
 
-    # Parse the html content
-    soup = BeautifulSoup(content, 'html.parser')   
+    # send a get request to url
+    r= requests.get(url)
+    r.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
 
+    url_status= r.status_code
+    print(url_status)
+    url_content= r.text # get url content as text
 
+    # find main content of the feature stories page
+    soup= BeautifulSoup(url_content, 'html.parser')
     main_content= soup.find("div", "region region-content")
+
+
+    # get news/stories from  URLS from Highligth page
     div_content= main_content.find("div", "view-content")
     h3_a_href= div_content.find_all("h3")
-
     
-    # # Navigation of the Feature/news Afro WHO website
-    
-    
-    # # Get news and stories from features_stories page
     base_url = "https://www.afro.who.int"
-    content_links= []
+    content_urls= []
 
     for h3 in h3_a_href:
         a_href= h3.find("a")
-        content_links.append(f"{base_url}{a_href.get("href")}")
+        content_urls.append(f"https://www.afro.who.int{a_href.get("href")}")
 
         
 
-    return content_links
+    return content_urls
 
 
 if __name__=="__main__":
-    url= "https://www.afro.who.int/news/feature_stories"
-    main(url)
+    main()
