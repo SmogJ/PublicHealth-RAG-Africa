bdd6f74c8e3b0deaf72c3b1a05c9e6ae96da7354
Jeffrey Esedo <jiesedo@gmail.com>
2025-09-16 15:01:07 +0100

    initial commit

---
+++ scripts/extract_html_update.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+
+# check if the most recent article is an update
+def main():
+    ...
+
+# get the date of the last scraped article
+def get_last_article():
+    ...
+
+# get the data from the url of the lastest html article
+def get_update():
+    ...
+
+
+if __name__=="__main__":
+    main()
